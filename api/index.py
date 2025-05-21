d53669c06e410d30745f55b1ee2da3a239489c1b
Aditya Vaidhya <aditya.r.vaidhya@gmail.com>
2025-05-21 14:00:31 +0530

    Fix: Python function structure

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ import json
 import os
 from urllib.parse import parse_qs
 
-def app(request, response):
+def handler(request, response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET"
 
@@ -14,4 +14,6 @@ def app(request, response):
         marks_data = json.load(f)
 
     result = [marks_data.get(name, None) for name in names]
-    return response.json({ "marks": result })
+    return response.json({"marks": result})
+
+app = handler  # required for Vercel to pick up
